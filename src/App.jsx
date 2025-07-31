2d6626ea6446999d2b49950bf7bc4b26d2bf1499
Md Rahat Islam <islammdrahat308@gmail.com>
2025-07-31 08:19:48 +0600

    header

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Root from "./components/Root";
+
+export default function App() {
+ return (
+  <div>
+   <Root />
+   <hr className="border-b border-gray-300" />
+  </div>
+ );
+}
---
+++ src/components/Root.jsx
@@ -0,0 +1,9 @@
+import TopBar from "./TopBar";
+
+export default function Root() {
+ return (
+  <div className="max-w-7xl mx-auto">
+   <TopBar />
+  </div>
+ );
+}
